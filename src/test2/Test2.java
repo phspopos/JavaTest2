3d1c0aff9352e5641c0f2e23a96a4f052b410c56
phspopos <phspopos@naver.com>
2025-03-28 16:34:18 +0900

    Merge remote-tracking branch 'origin/main' into main

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -3,6 +3,10 @@ package test2;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("또다시 한번");
+		System.out.println("77또다시 한번");
+		System.out.println("77또다시 한번");
+		System.out.println("77또다시 한번");
+		System.out.println("77또다시 한번");
+		
 	}
 }
